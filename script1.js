21aa591a3d7a7013faea4c8b43f2030e9f71d300
leo <shadyslim713@gmail.com>
2023-02-09 15:25:06 +0630

    thirdcommitformJunior

--- script1.js
+++ script1.js
@@ -1,146 +1,43 @@
-const startBtn = document.querySelector(".start-btn");
-const endBtn = document.querySelector(".end-btn");
-const redLig = document.querySelector(".redlight");
-const yellowLig = document.querySelector(".yellowlight");
-const greenLig = document.querySelector(".greenlight");
+const btnStart = document.querySelector(".btn-primary");
+const btnStop = document.querySelector(".btn-danger");
+const redTag = document.querySelector(".red");
+const yellowTag = document.querySelector(".yellow");
+const greenTag = document.querySelector(".green");
+let second = 0;
+let stop;
 
-let red = 0,
-  green = 0,
-  yellow = 0;
+const run = () => {
+  yellowTag.style.opacity = 0;
+  greenTag.style.opacity = 0;
 
-let Rsett;
-let Ysett;
-let Gsett;
+  setTimeout(() => {
+    redTag.style.opacity = 0;
+    yellowTag.style.opacity = 1;
+  }, 5000);
 
-// START BUTTON
-startBtn.addEventListener("click", () => {
-  Rsett = setInterval(showRed, 1000);
-  RRR = setInterval(showSecRed, 1000);
+  setTimeout(() => {
+    yellowTag.style.opacity = 0;
+    greenTag.style.opacity = 1;
+  }, 7000);
+
+  setTimeout(() => {
+    greenTag.style.opacity = 0;
+    redTag.style.opacity = 1;
+  }, 12000);
+};
+
+const start = () => {
+  stop = setInterval(() => {
+    second++;
+    if (second === 1) {
+      run();
+      second = -12;
+      console.log(second);
+    }
+  }, 1000);
+};
+
+btnStart.addEventListener("click", start);
+btnStop.addEventListener("click", () => {
+  clearInterval(stop);
 });
-
-// END BUTTON
-endBtn.addEventListener("click", () => {
-  (red = 0), (green = 0), (yellow = 0);
-  redLig.textContent = "00";
-  yellowLig.textContent = "00";
-  greenLig.textContent = "00";
-  clearInterval(Rsett);
-  clearInterval(Ysett);
-  clearInterval(Gsett);
-  redLig.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-  yellowLig.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-  greenLig.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-});
-// RED
-const showRed = () => {
-  red += 1;
-  redLig.style.backgroundColor = "red";
-  if (red === 11) {
-    redLig.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-    red = 0;
-    clearInterval(Rsett);
-    Ysett = setInterval(showYellow, 1000);
-  }
-  const redd = red < 10 ? "0" + red.toString() : red;
-  redLig.textContent = redd;
-};
-// YELLOW
-const showYellow = () => {
-  yellow += 1;
-  yellowLig.style.backgroundColor = "yellow";
-  if (yellow === 6) {
-    yellowLig.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-    yellow = 0;
-    clearInterval(Ysett);
-    Gsett = setInterval(showGreen, 1000);
-  }
-  const yelloww = yellow < 10 ? "0" + yellow.toString() : yellow;
-  yellowLig.textContent = yelloww;
-};
-// GREEN
-const showGreen = () => {
-  green += 1;
-  greenLig.style.backgroundColor = "greenyellow";
-  if (green === 11) {
-    greenLig.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-    green = 0;
-    clearInterval(Gsett);
-    Rsett = setInterval(showRed, 1000);
-  }
-  const greenn = green < 10 ? "0" + green.toString() : green;
-  greenLig.textContent = greenn;
-};
-
-// ***************************************************************************************************
-const redTrackDiv = document.querySelector(".redTrack");
-const yellowTrackDiv = document.querySelector(".yellowTrack");
-const greenTrackDiv = document.querySelector(".greenTrack");
-const secStartBtn = document.querySelector(".twoStart-btn");
-const secEndBtn = document.querySelector(".twoEnd-btn");
-const numbers = document.querySelector(".number");
-let reded = 0,
-  yellowed = 0,
-  greened = 0;
-
-let RRR;
-let YYY;
-let GGG;
-
-// STARTED
-// secStartBtn.addEventListener("click", () => {
-//   RRR = setInterval(showSecRed, 1000);
-// });
-
-// ENDED
-secEndBtn.addEventListener("click", () => {
-  (reded = 0), (yellowed = 0), (greened = 0);
-  numbers.textContent = "00";
-  clearInterval(RRR);
-  clearInterval(YYY);
-  clearInterval(GGG);
-  redTrackDiv.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-  yellowTrackDiv.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-  greenTrackDiv.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-});
-
-const showSecRed = () => {
-  reded += 1;
-  redTrackDiv.style.backgroundColor = "red";
-  if (reded === 11) {
-    redTrackDiv.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-    reded = 0;
-    clearInterval(RRR);
-    YYY = setInterval(showSecYellow, 1000);
-  }
-  const numText = reded < 10 ? "0" + reded.toString() : reded;
-  numbers.textContent = numText;
-};
-
-const showSecYellow = () => {
-  yellowed += 1;
-  yellowTrackDiv.style.backgroundColor = "yellow";
-  if (yellowed === 6) {
-    yellowTrackDiv.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-    yellowed = 0;
-    clearInterval(YYY);
-    GGG = setInterval(showSecGreen, 1000);
-  }
-  const numText = yellowed < 10 ? "0" + yellowed.toString() : yellowed;
-  numbers.textContent = numText;
-};
-
-const showSecGreen = () => {
-  greened += 1;
-  greenTrackDiv.style.backgroundColor = "greenyellow";
-  if (greened === 11) {
-    greenTrackDiv.style.backgroundColor = "rgba(89, 89, 105, 0.526)";
-    greened = 0;
-    clearInterval(GGG);
-    RRR = setInterval(showSecRed, 1000);
-  }
-  const numText = greened < 10 ? "0" + greened.toString() : greened;
-  numbers.textContent = numText;
-};
-
-console.log("Hello world");
-console.log("hello github");
